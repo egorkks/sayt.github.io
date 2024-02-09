1cce64682266c64d0053560a010a74954beb10b6
egorkks.github.io <155973338+egorkks@users.noreply.github.com>
2024-02-09 11:59:11 -0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-// script.js
+
 
 document.getElementById('accessButton').addEventListener('click', function () {
     var userCode = document.getElementById('accessCode').value;
 
-    // Замените 'ваш_ожидаемый_код' на реальное ожидаемое значение кода пользователя
+  
     if (userCode === '1111') {
-        // Если код верен, отобразите блок mainFunctionality и скройте форму доступа
+    
         document.getElementById('mainFunctionality').style.display = 'block';
         document.getElementById('accessForm').style.display = 'none';
     } else {
         alert('Неверный код доступа');
     }
 });
-
-// Дополнительный код для основной функциональности, если необходимо
-// Например, обновление информации в блоке mainFunctionality
