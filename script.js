accd6861d20b047335fb4c5338c559bb2000867b
egorkks.github.io <155973338+egorkks@users.noreply.github.com>
2024-02-09 12:01:12 -0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const accessForm = document.getElementById('accessForm');
+    const accessButton = document.getElementById('accessButton');
+    const selectedServiceInfo = document.getElementById('selectedServiceInfo');
 
+    let selectedService = '';
 
-document.getElementById('accessButton').addEventListener('click', function () {
-    var userCode = document.getElementById('accessCode').value;
+    accessForm.addEventListener('submit', function (event) {
+        event.preventDefault(); // Предотвращаем стандартное поведение формы
 
-  
-    if (userCode === '1111') {
+        const accessCodeInput = document.getElementById('accessCode');
+        const accessCode = accessCodeInput.value;
+
+        // Пример: Проверка кода (ваша логика проверки должна быть на сервере)
+        if (accessCode === '1111') {
+
+            // Сохраняем выбор услуги в localStorage
+            localStorage.setItem('selectedService', selectedService);
+
+            // Перенаправляем пользователя на другую страницу в папке system1
+            window.location.href = "file:///C:/Users/%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C/Desktop/%D0%97%D0%B0%D0%BA%D0%B0%D0%B7/system/system1/main_functionality.html"
+            return true; // Разрешаем отправку формы
+        } else {
+            alert('Неправильный код доступа. Попробуйте снова.');
+            return false; // Запрещаем отправку формы
+        }
+    });
+
+    const serviceButtons = document.querySelectorAll('input[name="service"]');
+    serviceButtons.forEach(function (button) {
+        button.addEventListener('change', function () {
+            selectedService = button.value; // Здесь сохраняется выбор пользователя
+        });
+    });
     
-        document.getElementById('mainFunctionality').style.display = 'block';
-        document.getElementById('accessForm').style.display = 'none';
-    } else {
-        alert('Неверный код доступа');
+    function toggleAboutUs() {
+        const aboutUsSection = document.getElementById('aboutUsSection');
+        aboutUsSection.style.display = aboutUsSection.style.display === 'none' ? 'block' : 'none';
+    }
+
+    function resetButtons() {
+        const aboutUsSection = document.getElementById('aboutUsSection');
+        aboutUsSection.style.display = 'none';
+    }
+
+    function redirectToTelegram() {
+        window.location.href = 'https://t.me/w3trr';
     }
 });
+
+
